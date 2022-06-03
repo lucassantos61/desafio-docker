434e5d24f67bbf60c013c1dd3eede91ea1feb680
Lucas <lfsantos@rd.com.br>
2022-06-03 12:01:14 -0300

    Adicionando desafio node

---
+++ desafio-nginx-node/app/index.js
@@ -0,0 +1,47 @@
+import http from 'http'
+import { createConnection } from 'mysql'
+
+const PORT = 3000
+
+const connection = createConnection({
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'full-cycle'
+})
+
+connection.connect(() => {
+    createTable()
+    console.log(`Database connected!!!`)
+})
+
+function createTable() {
+    connection.query(`
+    CREATE TABLE IF NOT EXISTS PEOPLE(
+        ID INT NOT NULL AUTO_INCREMENT, 
+        NAME VARCHAR(50) NOT NULL,
+        PRIMARY KEY (ID)
+)`)
+}
+
+let indexPeople = 0
+const server = http.createServer((req, res) => {
+    connection.query(`INSERT INTO PEOPLE (name) values ("Lucão do PHP Rules")`, (err, results, _) => {
+        connection.query(`SELECT * FROM PEOPLE as people`, (err, results, _) => {
+            res.end(response(results))
+            indexPeople++
+        })
+    })
+})
+
+function response(results) {
+    console.log(results)
+    return `
+    <h1>Full Cycle Rocks!</h1>
+    <ul>
+        ${results.map(({ NAME }) => `<li>${NAME}</li>`)}
+    </ul>
+    `
+}
+
+server.listen(PORT, () => console.log(`Server is running at ${PORT}`))
